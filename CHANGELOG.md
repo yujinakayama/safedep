91a45243898374d233c6ded760d5ba90553713cd
Yuji Nakayama <nkymyj@gmail.com>
2015-01-10 18:39:57 +0900

    Bump version to 0.0.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,8 @@
 
 ## Development
 
+## v0.0.2
+
 * Fix an error when a `Gemfile` includes `bundler` dependency without version specifier.
 
 ## v0.0.1
--- lib/safedep/version.rb
+++ lib/safedep/version.rb
@@ -3,7 +3,7 @@ module Safedep
   module Version
     MAJOR = 0
     MINOR = 0
-    PATCH = 1
+    PATCH = 2
 
     def self.to_s
       [MAJOR, MINOR, PATCH].join('.')
