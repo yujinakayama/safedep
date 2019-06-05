41308852168a7321e33f512c0c896e99876e00fe
Yuji Nakayama <nkymyj@gmail.com>
2019-06-05 15:42:51 +0900

    Bump version to 0.4.0

--- lib/safedep/version.rb
+++ lib/safedep/version.rb
@@ -2,8 +2,8 @@ module Safedep
   # http://semver.org/
   module Version
     MAJOR = 0
-    MINOR = 3
-    PATCH = 2
+    MINOR = 4
+    PATCH = 0
 
     def self.to_s
       [MAJOR, MINOR, PATCH].join('.')
